a3c647096705a1010cdaa625cdcd45ca29c20025
MisterETS <1595043@Students.wits.ac.za>
2022-11-06 16:34:42 +0200

    This is for coding, note that it is incompltete as aspects of the fighting are still underway

---
+++ Special Cryptid Protocol/Assets/Scripts/2d fight/item/BattleSystem.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+	public GameObject playerPrefab;
+	public GameObject enemyPrefab;
+
+	public Transform playerBattleStation;
+	public Transform enemyBattleStation;
+
+	Unit playerUnit;
+	Unit enemyUnit;
+
+	public TMP_Text dialogueText;
+
+	public BattleHUD playerHUD;
+	public BattleHUD enemyHUD;
+
+	public BattleState state;
+
+	
+
+	private int Checker = 0;
+
+    public GameObject Heart;
+    private Vector3 Startingpos;
+
+
+    public float speed;
+    public float Sensitivity;
+    private Vector2 MovePos;
+	public int MaxX; 
+    public int MaxY ;
+    public int MinX ;
+    public int MinY ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		state = BattleState.START;
+		StartCoroutine(SetupBattle());
+    }
+
+	IEnumerator SetupBattle()
+	{
+		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+		playerUnit = playerGO.GetComponent<Unit>();
+
+		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+		enemyUnit = enemyGO.GetComponent<Unit>();
+
+		dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
+
+		playerHUD.SetHUD(playerUnit);
+		enemyHUD.SetHUD(enemyUnit);
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.PLAYERTURN;
+		PlayerTurn();
+	}
+
+	IEnumerator PlayerAttack()
+	{
+		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+		enemyHUD.SetHP(enemyUnit.currentHP);
+		dialogueText.text = "The attack is successful!";
+
+		yield return new WaitForSeconds(2f);
+
+		if(isDead)
+		{
+			state = BattleState.WON;
+			EndBattle();
+		} else
+		{
+			state = BattleState.ENEMYTURN;
+			StartCoroutine(EnemyTurn());
+		}
+	}
+
+	IEnumerator EnemyTurn()
+	{
+		int CurrentHealth = playerUnit.currentHP;
+		dialogueText.text = enemyUnit.unitName + " attacks!";
+        Startingpos = new Vector3(-6, 2, 423);
+        transform.position = Startingpos;
+		MovePos = Startingpos;
+		MaxX = 100;
+		MaxY = 100;
+		MinX = -100;
+		MinY = -100;
+        Checker = 1;
+
+
+
+
+        yield return new WaitForSeconds(30f);
+		Checker = 0;	
+
+		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+
+		yield return new WaitForSeconds(1f);
+
+		if(isDead)
+		{
+			state = BattleState.LOST;
+			EndBattle(); 
+			
+		} else
+		{
+			state = BattleState.PLAYERTURN;
+			PlayerTurn();
+		}
+
+	}
+
+	void EndBattle()
+	{
+		if(state == BattleState.WON)
+		{
+			dialogueText.text = "You won the battle!";
+		} else if (state == BattleState.LOST)
+		{
+			dialogueText.text = "You were defeated.";
+		}
+	}
+
+	void PlayerTurn()
+	{
+		dialogueText.text = "Choose an action:";
+	}
+
+	IEnumerator PlayerHeal()
+	{
+		playerUnit.Heal(5);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+		dialogueText.text = "You feel renewed strength!";
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.ENEMYTURN;
+		StartCoroutine(EnemyTurn());
+	}
+
+	public void OnAttackButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerAttack());
+	}
+
+	public void OnHealButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerHeal());
+	}
+
+	private void FixedUpdate()
+	{
+		if (Checker == 1)
+		{
+            float horizontal = Input.GetAxis("Horizontal") * Sensitivity;
+            float vertical = Input.GetAxis("Vertical") * Sensitivity;
+            MovePos.x += horizontal;
+            MovePos.y += vertical;
+
+            MovePos.x = Mathf.Clamp(MovePos.x, MinX, MaxX);
+            MovePos.y = Mathf.Clamp(MovePos.y, MinY, MaxY);
+
+            Heart.transform.position = Vector2.Lerp(transform.position, MovePos, speed * Time.deltaTime);
+        }
+        
+    }
+
+}
---
+++ Special Cryptid Protocol/Assets/Scripts/2d fight/item/HeartCtrl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCtrl : MonoBehaviour
+{
+    private GameObject Heart;
+    private Vector3 Startingpos;
+     
+
+    public float speed;
+    public float Sensitivity;
+    private Vector2 MovePos;
+    public int MaxX = 2;
+    public int MaxY = 2;
+    public int MinX = -2;
+    public int MinY = -2;
+
+    
+
+
+   
+
+   
+
+}
